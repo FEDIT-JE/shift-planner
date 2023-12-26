8b7a24962910b3af276d5369a0a3ed73fb942f1c
jx4n16 <94107361+FEDIT-JE@users.noreply.github.com>
2023-12-26 19:12:33 +0100

    header + footer start

---
+++ shift-planner/src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.css';
+
+const Footer = () => {
+  return (
+        <div className="footer">
+          <p class="logo">ShiftFlex </p>
+        </div>
+  )
+}
+
+export default Footer
+
+
